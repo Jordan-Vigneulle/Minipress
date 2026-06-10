332ebc644161aaa14dcfbb9e1cf3531a53e2efe2
Jordan Vigneulle <e76320u@etu.univ-lorraine.fr>
2026-06-10 14:20:20 +0200

    🚧

--- minipress.core/src/conf/routes.php
+++ minipress.core/src/conf/routes.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use minipress\appli\webui\actions\AccueilAction;
+use minipress\appli\webui\actions\Article\ArticleCreateAction;
+use minipress\appli\webui\actions\Article\ArticleStoreAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -10,6 +12,8 @@ return function (\Slim\App $app): \Slim\App {
 
     $app->get('/', AccueilAction::class)->setName('home');
 
-   
+    $app->get('/article/create', ArticleCreateAction::class)->setName('articleCreate');
+    $app->post('/article/create', ArticleStoreAction::class)->setName('articleStore');
+
     return $app;
 };

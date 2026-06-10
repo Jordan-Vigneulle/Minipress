69adcb48a505115595bcd87e70eb07e4977f1f11
Maryam Tahri <e88814u@etu.univ-lorraine.fr>
2026-06-10 11:57:35 +0200

    Ajout de la class API permettant d'afficher les catégorie au format JSON

---
+++ minipress.core/src/api/actions/CategoriesAction.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace minipress\appli\api\actions;
+
+use minipress\appli\application_core\application\useCases\categorie\GestionCategorie;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class CategoriesAction
+{
+    public function __invoke(
+        Request $request,
+        Response $response,
+        array $args
+    ): Response {
+
+        $service = new GestionCategorie();
+        $categories = $service->getCategories();
+        
+        $response->getBody()->write(json_encode($categories));
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(200);
+    }
+}

23440c9b023e137eed099a5cbf397c11f162da46
Jordan Vigneulle <e76320u@etu.univ-lorraine.fr>
2026-06-10 15:07:18 +0200

    ✨ Ajout d'un selecteur de catégorie

--- minipress.core/src/webui/actions/Article/ArticleCreateAction.php
+++ minipress.core/src/webui/actions/Article/ArticleCreateAction.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace minipress\appli\webui\actions\Article;
 
 
+use minipress\appli\application_core\application\useCases\categorie\GestionCategorie;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Flash\Messages;
@@ -17,6 +18,7 @@ class ArticleCreateAction
         $twig = Twig::fromRequest($request);
         return $twig->render($response, 'Article/articleCreationTwig.twig', [
             'csrf_token' => $_SESSION['csrf_article'] = bin2hex(random_bytes(32)),
+            'categories' => GestionCategorie::getCategories(),
         ]);
     }
 }

24f6ab1043dab8dc04ce1b45cd11a46a2a4491d5
Auriane Guyot <aurianeguyot70@gmail.com>
2026-06-10 11:32:00 +0200

    Ajout des routes catégories

--- minipress.core/src/conf/routes.php
+++ minipress.core/src/conf/routes.php
@@ -2,13 +2,21 @@
 
 declare(strict_types=1);
 
-use minipress\appli\webui\actions\AccueilAction;
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+
+use minipress\appli\webui\actions\AccueilAction;
+use minipress\appli\webui\actions\Categories\CategoriesListeAction;
+use minipress\appli\webui\actions\Categories\ArticlesParCategorieAction;
 
 return function (\Slim\App $app): \Slim\App {
 
     $app->get('/', AccueilAction::class)->setName('home');
+
+    // Catégories
+    $app->get('/categories', CategoriesListeAction::class)->setName('allCategories');
+    $app->get('/categories/{id}/articles', ArticlesParCategorieAction::class)->setName('allArticlesByCategorie');
 
    
     return $app;

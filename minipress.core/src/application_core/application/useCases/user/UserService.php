b96529dd3d9ac100638ce56512987d3914e9bcd1
Maryam Tahri <e88814u@etu.univ-lorraine.fr>
2026-06-10 16:31:59 +0200

    Ajout de la methode permettant de recuperer les article d'un utilisateur dans le service user

--- minipress.core/src/application_core/application/useCases/user/UserService.php
+++ minipress.core/src/application_core/application/useCases/user/UserService.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace minipress\appli\application_core\application\useCases\user;
 
 use minipress\appli\application_core\domain\entities\Utilisateur;
+use minipress\appli\application_core\domain\entities\Article;
 
 class UserService implements UserServiceInterface
 {
@@ -27,5 +28,23 @@ class UserService implements UserServiceInterface
     {
         $user->chemin_acces_img = $cheminAccesImg;
         $user->save();
+    }
+
+
+    public function getArticlesByUser(int $user_id): array {
+        $user = Utilisateur::where('id', $user_id)->first();
+        if (!$user) {
+            throw new \Exception("Aucun utilisateur trouvé avec l'identifiant $user_id");
+        }
+
+        $articles = Article::where('id_utilisateur', $user_id)->get();
+        if ($articles->isEmpty()) {
+            throw new \Exception("Aucun article trouvé pour l'auteur $user_id");
+        }
+
+        return [
+            'auteur' => $user->toArray(),
+            'articles' => $articles->toArray(),
+        ];
     }
 }

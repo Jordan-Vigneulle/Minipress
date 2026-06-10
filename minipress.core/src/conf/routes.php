9f4d5c118c52f32d988d647a899448b93dd5bff7
Thibaut <thibaut.louyot5@etu.univ-lorraine.fr>
2026-06-10 14:20:31 +0200

    feat: ajout des routes /admin/articles et /admin/articles/{id}/toggle-publish

--- minipress.core/src/conf/routes.php
+++ minipress.core/src/conf/routes.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use minipress\appli\webui\actions\AccueilAction;
+use minipress\appli\webui\actions\Articles\ListArticlesAction;
+use minipress\appli\webui\actions\Articles\TogglePublishAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -10,6 +12,8 @@ return function (\Slim\App $app): \Slim\App {
 
     $app->get('/', AccueilAction::class)->setName('home');
 
-   
+    $app->get('/admin/articles', ListArticlesAction::class)->setName('admin_liste_articles');
+    $app->post('/admin/articles/{id}/toggle-publish', TogglePublishAction::class)->setName('admin_toggle_publish');
+
     return $app;
 };

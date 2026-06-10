6e46afb665a5d7d57200a4a4830633dee8e9f222
Thibaut <thibaut.louyot5@etu.univ-lorraine.fr>
2026-06-10 14:05:01 +0200

    feat: ajout de l'action qui gère si un article est publié ou non

---
+++ minipress.core/src/webui/actions/Articles/TogglePublishAction.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace minipress\appli\webui\actions\Articles;
+
+use minipress\appli\application_core\application\useCases\article\ArticleService;
+use minipress\appli\application_core\application\useCases\article\ArticleServiceInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Routing\RouteContext;
+
+class TogglePublishAction
+{
+    private ArticleServiceInterface $articleService;
+
+    public function __construct()
+    {
+        $this->articleService = new ArticleService();
+    }
+
+    public function __invoke(Request $request, Response $response, array $args): Response
+    {
+        $id = $args['id'] ?? null;
+        if ($id !== null) {
+            try {
+                $this->articleService->basculerPublication((int)$id);
+            } catch (\RuntimeException $e) {
+                // @TODO: Gérer l'exception si l'article n'est pas trouvé ou autre erreur
+            }
+        }
+
+        $routeParser = RouteContext::fromRequest($request)->getRouteParser();
+        $redirectUrl = $routeParser->urlFor('admin_liste_articles');
+
+        return $response
+            ->withHeader('Location', $redirectUrl)
+            ->withStatus(302);
+    }
+}

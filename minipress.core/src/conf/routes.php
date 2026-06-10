fb7b0a58c473c8991c3625f4762aa01589f6ec81
Maryam Tahri <e88814u@etu.univ-lorraine.fr>
2026-06-10 11:58:12 +0200

    Ajout de la route liée a l'API pour affihcer les catégorie

--- minipress.core/src/conf/routes.php
+++ minipress.core/src/conf/routes.php
@@ -9,6 +9,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use minipress\appli\webui\actions\AccueilAction;
 use minipress\appli\webui\actions\Categories\CategoriesListeAction;
 use minipress\appli\webui\actions\Categories\ArticlesParCategorieAction;
+use minipress\appli\api\actions\CategoriesAction;
 
 return function (\Slim\App $app): \Slim\App {
 
@@ -18,6 +19,11 @@ return function (\Slim\App $app): \Slim\App {
     $app->get('/categories', CategoriesListeAction::class)->setName('allCategories');
     $app->get('/categories/{id}/articles', ArticlesParCategorieAction::class)->setName('allArticlesByCategorie');
 
+    
+
+
+    // API
+    $app->get('/api/categories', CategoriesAction::class)->setName('api_AllCategories');
    
     return $app;
 };

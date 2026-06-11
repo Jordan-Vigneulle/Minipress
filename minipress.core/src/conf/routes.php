7fb90e47686f34ecbb6a17b45573183234d859ba
Nathan Yvon <nathan.yvon3@gmail.com>
2026-06-11 04:31:15 +0200

    Ajout : Ajout de mes routes pour Utilisateur (login / logout / createUser / loginPage) + Profil

--- minipress.core/src/conf/routes.php
+++ minipress.core/src/conf/routes.php
@@ -24,6 +24,16 @@ use minipress\appli\api\actions\ArticleParIDAction as API_ArticleParIDAction;
 use minipress\appli\api\actions\ArticlesAction as API_ListArticlesAction;
 use minipress\appli\api\actions\ArticlesParAuteurAction as API_ArticlesParAuteurAction;
 
+use minipress\appli\webui\actions\Authentification\AuthConnectionAction;
+use minipress\appli\webui\actions\Authentification\AuthInscriptionAction;
+use minipress\appli\webui\actions\Authentification\AuthLoginAction;
+use minipress\appli\webui\actions\Authentification\AuthRegisterAction;
+use minipress\appli\webui\actions\Authentification\AuthDeconnectionAction;
+use minipress\appli\webui\actions\Profils\ProfileAction;
+use minipress\appli\webui\actions\Profils\ProfilsChangementMDPAction;
+use minipress\appli\webui\actions\Profils\ProfilsChangementPseudoAction;
+use minipress\appli\webui\actions\Profils\ProfilsChangementAvatarAction;
+
 return function (\Slim\App $app): \Slim\App {
 
     $app->get('/', AccueilAction::class)->setName('home');
@@ -46,9 +56,22 @@ return function (\Slim\App $app): \Slim\App {
     $app->get('/api/articles/{id}', API_ArticleParIDAction::class)->setName('api_OneArticle');
     $app->get('/api/articles', API_ListArticlesAction::class)->setName('api_ListeArticles');
     $app->get('/api/auteurs/{id}/articles', API_ArticlesParAuteurAction::class)->setName('api_AllArticlesByAuteur');
-   
+
     $app->get('/article/create', ArticleCreateAction::class)->setName('articleCreate');
     $app->post('/article/create', ArticleStoreAction::class)->setName('articleStore');
+
+    // Authentification
+    $app->get('/pageLogin', AuthConnectionAction::class)->setName('loginPage');
+    $app->post('/login', AuthLoginAction::class)->setName('login');
+    $app->get('/pageCreateUser', AuthInscriptionAction::class)->setName('createUserPage');
+    $app->post('/register', AuthRegisterAction::class)->setName('register');
+    $app->get('/logout', AuthDeconnectionAction::class)->setName('logout');
+
+    // Profil
+    $app->get('/profil', ProfileAction::class)->setName('profil');
+    $app->post('/profil/avatar', ProfilsChangementAvatarAction::class)->setName('changeAvatar');
+    $app->post('/profil/password', ProfilsChangementMDPAction::class)->setName('changePassword');
+    $app->post('/profil/username', ProfilsChangementPseudoAction::class)->setName('changeUsername');
 
     return $app;
 };

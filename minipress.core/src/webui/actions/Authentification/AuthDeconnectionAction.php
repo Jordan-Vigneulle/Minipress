d441aedfe52746e2b5ef77d3ef42848fc4d5b5e4
Nathan Yvon <nathan.yvon3@gmail.com>
2026-06-11 04:41:28 +0200

    Ajout : declare(strict_types=1)

--- minipress.core/src/webui/actions/Authentification/AuthDeconnectionAction.php
+++ minipress.core/src/webui/actions/Authentification/AuthDeconnectionAction.php
@@ -1,15 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace minipress\appli\webui\actions\Authentification;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Routing\RouteContext;
 
-class AuthDeconnectionAction {
-    public function __invoke(Request $request, Response $response, array $args): Response {
+class AuthDeconnectionAction
+{
+    public function __invoke(Request $request, Response $response, array $args): Response
+    {
         $routeParser = RouteContext::fromRequest($request)->getRouteParser();
         session_destroy();
-        return $response->withHeader('Location',$routeParser->urlFor('home'))->withStatus(302);
+        return $response->withHeader('Location', $routeParser->urlFor('home'))->withStatus(302);
     }
 }

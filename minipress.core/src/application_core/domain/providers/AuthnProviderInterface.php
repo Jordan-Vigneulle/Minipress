f36d33d8b675885168bca83e8b58edb84d344077
Nathan Yvon <nathan.yvon3@gmail.com>
2026-06-11 04:26:53 +0200

    Ajout : Rajout du provider AuthnProvider et de son interface pour la gestion de la session utilisateur

---
+++ minipress.core/src/application_core/domain/providers/AuthnProviderInterface.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace minipress\appli\application_core\domain\providers;
+
+use minipress\appli\application_core\domain\entities\Utilisateur;
+
+interface AuthnProviderInterface
+{
+    public function getSignedInUser(): ?Utilisateur;
+
+    public function signin(string $email, string $password): ?Utilisateur;
+}

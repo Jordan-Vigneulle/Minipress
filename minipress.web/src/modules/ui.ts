248350e7f95828c4cf02b642a1d66935881d160b
Maryam Tahri <e88814u@etu.univ-lorraine.fr>
2026-06-12 13:59:29 +0200

    Ajout des methode display pour afficher les differente fonctionnalité (les 5 premiere issue

--- minipress.web/src/modules/ui.ts
+++ minipress.web/src/modules/ui.ts
@@ -1,1 +1,30 @@
 'use strict';
+import Handlebars from 'handlebars';
+
+export function displayArticleOrderby(articles: any) {
+    const templateArticles = Handlebars.compile(document.querySelector<HTMLScriptElement>('#articleOrderbyTemplate')!.innerHTML);
+    document.querySelector('#les_articles_orderby')!.innerHTML = templateArticles({ articles });
+}
+
+export function displayCategories(categories: any) {
+    const templateCategories = Handlebars.compile(document.querySelector<HTMLScriptElement>('#categorieTemplate')!.innerHTML);
+
+    document.querySelector('#la_categorie')!.innerHTML = templateCategories({ categories });
+}
+
+export function displayArticleByCategorie(data: any) {
+    const template = Handlebars.compile(document.querySelector<HTMLScriptElement>('#articleByCategorieTemplate')!.innerHTML);
+    document.querySelector('#les_articles_par_categorie')!.innerHTML = template(data); // data = { categorie, articles }
+}
+
+export function displayArticle(article: any) {
+    const templateArticles = Handlebars.compile(document.querySelector<HTMLScriptElement>('#articleTemplate')!.innerHTML);
+    document.querySelector('#un_article')!.innerHTML = templateArticles({ article });
+}
+
+export function displayArticleByUser(data: any) {
+    const template = Handlebars.compile(document.querySelector<HTMLScriptElement>('#articleByUserTemplate')!.innerHTML);    
+    document.querySelector('#les_articles_par_user')!.innerHTML = template(data); // data = { auteur, articles }
+}
+
+

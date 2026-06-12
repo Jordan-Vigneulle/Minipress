8b6de20ede067373c7b2af6ebe907c289fcbbe24
Maryam Tahri <e88814u@etu.univ-lorraine.fr>
2026-06-12 08:21:43 +0200

    Création des type CAtegorie Categories et Article

--- minipress.web/src/modules/types.ts
+++ minipress.web/src/modules/types.ts
@@ -1,5 +1,26 @@
 'use strict';
 
+export interface Categorie {
+    id: number;
+    titre: string;
+    articles: Article[];
+}
+
+export interface Categories {
+    categories: Categorie[];
+}
+
+export interface Article {
+    id: number;
+    titre: string;
+    resume: string;
+    contenu: string;
+    date: Date;
+    id_categorie: number;
+    id_utilisateur: number;
+    est_publie: boolean;
+}
+
 
 
 

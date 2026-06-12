d3ca1c8ee14ea7717aa9d2cb07e7ced40ca0659e
Maryam Tahri <e88814u@etu.univ-lorraine.fr>
2026-06-12 08:21:43 +0200

    Création des type CAtegorie Categories et Article

--- minipress.web/src/modules/config.ts
+++ minipress.web/src/modules/config.ts
@@ -2,3 +2,5 @@
 
 export const url = 'http://localhost:4490/api';
 export const url_categories = 'http://localhost:4490/api/categories'
+export const url_articles = 'http://localhost:4490/api/articles';
+

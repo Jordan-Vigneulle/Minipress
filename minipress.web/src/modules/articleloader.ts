3501d72c381457f0f72a88260b616566b8033f74
Maryam Tahri <e88814u@etu.univ-lorraine.fr>
2026-06-12 14:00:48 +0200

    Mise en lace de la methode load all pour charger les api

--- minipress.web/src/modules/articleloader.ts
+++ minipress.web/src/modules/articleloader.ts
@@ -1,4 +1,6 @@
 'use strict';
+import { url_categories } from './config';
+
 
 
 export async function loadAll<T>(url: string, uri?: string, idPicture?: string): Promise<T> {
@@ -10,7 +12,7 @@ export async function loadAll<T>(url: string, uri?: string, idPicture?: string):
         fullUrl = url;
     }
 
-    return fetch(fullUrl, { credentials: 'include' })
+    return fetch(fullUrl)
         .then((response: Response): Promise<T> => {
             if (response.ok) {
                 return response.json() as Promise<T>;
@@ -24,3 +26,19 @@ export async function loadAll<T>(url: string, uri?: string, idPicture?: string):
             return Promise.reject(error);
         });
 }
+
+// export async function loadCategories<T>(id: number): Promise<T> {
+//     return fetch(url_categories)
+//         .then((response: Response): Promise<T> => {
+//             if (response.ok) {
+//                 return response.json() as Promise<T>;
+//             }
+//             console.log('Response error : ' + response.status);
+//             return Promise.reject(new Error(response.statusText));
+//         })
+//         .catch((error: Error) => {
+//             console.log("Erreur : " + error);
+//             return Promise.reject(error);
+//         });
+// }
+    

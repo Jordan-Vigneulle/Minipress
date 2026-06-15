197a287275f0eb81adf5a2fd2d8faf8bc2e27150
Auriane Guyot <aurianeguyot70@gmail.com>
2026-06-15 11:40:00 +0200

    Modification des types

--- minipress.web/src/modules/types.ts
+++ minipress.web/src/modules/types.ts
@@ -3,7 +3,6 @@
 export interface Categorie {
     id: number;
     titre: string;
-
 }
 
 export interface Categories {
@@ -19,6 +18,10 @@ export interface Article {
     id_categorie: number;
     id_utilisateur: number;
     est_publie: boolean;
+    utilisteur : {
+        id: number,
+        pseudo: string;
+    };
 }
 
 export interface User{
@@ -26,7 +29,7 @@ export interface User{
     email: string;
     role: number;
     chemin_acces_img: string;
-    pseudo: string | null;
+    pseudo: string;
 }
 
 

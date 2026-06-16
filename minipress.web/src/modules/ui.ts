fb0d49147fbd7545d1bb6a679d0b0b809784ad45
Maryam Tahri <e88814u@etu.univ-lorraine.fr>
2026-06-16 09:18:13 +0200

    Remise des type dans les document (changment des any)

--- minipress.web/src/modules/ui.ts
+++ minipress.web/src/modules/ui.ts
@@ -2,56 +2,57 @@
 import Handlebars from 'handlebars';
 import { afficherModeArticle } from './modearticle';
 import { markdownToHtml } from "ts-markdown-parser";
+import { Article, Categorie } from './types';
 
 
-export function displayCategories(categories: any) {
+export function displayCategories(categories: Categorie[]): void {
     const templateCategories = Handlebars.compile(document.querySelector<HTMLScriptElement>('#categorieTemplate')!.innerHTML);
 
     document.querySelector('#la_categorie')!.innerHTML = templateCategories({ categories });
 }
 
-export function displayArticle(article: any) {
+export function displayArticle(article: Article): void {
     const templateArticles = Handlebars.compile(document.querySelector<HTMLScriptElement>('#articleTemplate')!.innerHTML);
     
     article.contenu = markdownToHtml(article.contenu, {});
-    article.date = formaterDate(article.date);
+    article.date = formaterDate(article.date );
 
     afficherModeArticle();
 
     document.querySelector('#un_article')!.innerHTML = templateArticles({ article });
 }
 
-export function displayArticleOrderby(articles: any) {
+export function displayArticleOrderby(articles: Article[]): void {
     const templateArticles = Handlebars.compile(document.querySelector<HTMLScriptElement>('#articleOrderbyTemplate')!.innerHTML);
 
-    const articlesAvecResume = articles.map((a: any) => ({
+    const articlesAvecResume = articles.map((a: Article) => ({
         ...a,
         resume: markdownToHtml(a.resume, {}),
-        date: formaterDate(a.date)
+        date: formaterDate(a.date )
     }));
  
     document.querySelector('#les_articles_orderby')!.innerHTML = templateArticles({ articles: articlesAvecResume });
 }
 
-export function displayArticleByCategorie(data: any) {
+export function displayArticleByCategorie(data: { articles: Article[] }): void {
     const template = Handlebars.compile(document.querySelector<HTMLScriptElement>('#articleByCategorieTemplate')!.innerHTML);
 
-    const articlesAvecResume = data.articles.map((a: any) => ({
+    const articlesAvecResume = data.articles.map((a: Article) => ({
         ...a,
         resume: markdownToHtml(a.resume, {}),
-        date: formaterDate(a.date)
+        date: formaterDate(a.date )
     }));
 
     document.querySelector('#les_articles_par_categorie')!.innerHTML = template({ ...data, articles: articlesAvecResume });
 }
 
-export function displayArticleByUser(data: any) {
+export function displayArticleByUser(data: { articles: Article[] }): void {
     const template = Handlebars.compile(document.querySelector<HTMLScriptElement>('#articleByUserTemplate')!.innerHTML);
 
-    const articlesAvecResume = data.articles.map((a: any) => ({
+    const articlesAvecResume = data.articles.map((a: Article) => ({
         ...a,
         resume: markdownToHtml(a.resume, {}),
-        date: formaterDate(a.date)
+        date: formaterDate(a.date )
     }));
     
     document.querySelector('#les_articles_par_user')!.innerHTML = template({ ...data, articles: articlesAvecResume });

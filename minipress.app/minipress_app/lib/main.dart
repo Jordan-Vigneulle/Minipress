e9144bac6c88ab95794697547afa02dcec75e004
Thibaut <thibaut.louyot5@etu.univ-lorraine.fr>
2026-06-16 14:18:08 +0200

    feat: ajout du thème sombre dans main.dart

--- minipress.app/minipress_app/lib/main.dart
+++ minipress.app/minipress_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'router.dart';
 
+final themeNotifier = ValueNotifier<ThemeMode>(ThemeMode.system);
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
@@ -11,14 +12,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Minipress',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
-        useMaterial3: true,
-      ),
-      routerConfig: router,
+    return ValueListenableBuilder<ThemeMode>(
+      valueListenable: themeNotifier,
+      builder: (context, currentThemeMode, child) {
+        return MaterialApp.router(
+          title: 'Minipress',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+            useMaterial3: true,
+          ),
+          darkTheme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.blueAccent,
+              brightness: Brightness.dark,
+            ),
+            useMaterial3: true,
+          ),
+          themeMode: currentThemeMode,
+          routerConfig: router,
+        );
+      },
     );
   }
 }
